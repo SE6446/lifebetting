760b21a250c98fe2224407f2d4e000d03256fbe3
Archie Macdonald <archiemac07@outlook.com>
2026-03-21 12:38:51 +0000

    worked on serving static

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-app.use(express.static('static'));
+app.use(express.static(__dirname + '/static'));
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+  res.sendFile(__dirname + '/static/index.html');
 });
 
 app.listen(port, () => {
