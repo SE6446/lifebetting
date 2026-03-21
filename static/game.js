057e280ff4d7298e28174ff0e635994bb0fbd643
EwanM5 <2681509@dundee.ac.uk>
2026-03-21 18:18:33 +0000

    Refactor game initialization and update HTML to call initializeGame on button click

--- static/game.js
+++ static/game.js
@@ -1,37 +1,23 @@
-const startBtn = document.getElementById('start-btn');
-const canvas = document.getElementById('game-canvas');
-const ctx = document.getElementById('marketChart').getContext('2d');
-import { ValueUpdater } from './render.js';
+console.log("1. Script file reached"); // This should show immediately
 
-let gameChart; // We define this here so we can update it later
-let score = 0;
+//window.onload = function() {
+//    console.log("2. Window loaded");
+//
+//    const startBtn = document.getElementById('start-btn');
+//    console.log("3. Button element:", startBtn);
+//
+//    if (!startBtn) {
+//        console.error("STOP: 'start-btn' not found in HTML. Check your IDs.");
+//        return;
+//    }
+//
+//    startBtn.addEventListener('click', () => {
+//        console.log("4. Button click detected!");
+//        // ... rest of your code
+//    });
+//};
 
-const valueUpdater = new ValueUpdater(1000, 100); // Update every second, starting at 100
-
-// 1. The Trigger: Everything starts when this is clicked
-startBtn.addEventListener('click', () => {
-    valueUpdater.start();
-    
-    startBtn.classList.add('d-none');
-    canvas.classList.remove('d-none');
-});
-
-
-// Function to update the chart whenever points are earned
-function updateMarket(points) {
-    score += points;
-    
-    // Add new data to chart
-    gameChart.data.labels.push(gameChart.data.labels.length);
-    gameChart.data.datasets[0].data.push(score);
-    
-    // Keep it looking like a "moving" stock ticker (last 20 points)
-    if (gameChart.data.labels.length > 20) {
-        gameChart.data.labels.shift();
-        gameChart.data.datasets[0].data.shift();
-    }
-    
-    gameChart.update();
-
- 
+function initializeGame() {
+    console.log("Initializing game...");
+    //const valueUpdater = new ValueUpdater(1000, 50);
 }
